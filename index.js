f01c064c86ee6eaacd213d65beb6902d06fe5006
Gagan Anand <gaganganand143@gmail.comm>
2025-01-01 23:20:37 +0530

    fixed port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const pdf = require('pdfkit');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
